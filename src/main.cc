74d248ec2a4fa674911126aaac3674fe254b9d19
Kenneth Blomqvist <kekeblom@gmail.com>
2021-01-06 21:56:36 +0100

    LineLayer

--- src/main.cc
+++ src/main.cc
@@ -11,6 +11,59 @@
 
 using namespace hud;
 
+std::array<float, 8> createVertices(const Point& start, const Point& end, float line_width) {
+  float w = line_width / 2.0;
+  return {
+    float(start.x), float(start.y) - w,
+    float(start.x), float(start.y) + w,
+    float(end.x), float(end.y) - w,
+    float(end.x), float(end.y) + w
+  };
+}
+
+static const uint16_t faces[] = {
+  0, 2, 3, 0, 3, 1
+};
+
+class LineLayer : public hud::views::View {
+protected:
+  bgfx::VertexBufferHandle vertex_buffer;
+  bgfx::IndexBufferHandle index_buffer;
+  bgfx::VertexLayout layout;
+  bgfx::ProgramHandle program;
+  std::array<float, 8> vertices;
+  Point start_point, end_point;
+  float line_width = 0.02;
+public:
+  LineLayer(int view_id) : View(view_id) {
+    start_point = Point(-1.0, 0.0);
+    end_point = Point(1.0, 0.0);
+
+    layout.begin()
+      .add(bgfx::Attrib::Position, 2, bgfx::AttribType::Float)
+      .end();
+
+    vertices = createVertices(start_point, end_point, line_width);
+    auto ref = bgfx::makeRef(vertices.data(), sizeof(float) * 8);
+    vertex_buffer = bgfx::createVertexBuffer(ref, layout);
+    index_buffer = bgfx::createIndexBuffer(bgfx::makeRef(faces, sizeof(faces)));
+    program = shader_utils::loadProgram("vs_line_layer", "fs_line_layer");
+  }
+
+  ~LineLayer() {
+    bgfx::destroy(vertex_buffer);
+    bgfx::destroy(index_buffer);
+    bgfx::destroy(program);
+  }
+
+  void render() const override {
+    bgfx::setVertexBuffer(0, vertex_buffer);
+    bgfx::setIndexBuffer(index_buffer);
+    bgfx::setState(BGFX_STATE_DEFAULT);
+    bgfx::submit(view_id, program);
+  }
+};
+
 class StereoLabel {
   private:
   AppWindow app;
@@ -31,13 +84,19 @@ class StereoLabel {
   protected:
   void createViews() {
     left_image_pane = std::make_shared<views::ImagePane>(0, "../assets/left.jpg");
-    right_pane = std::make_shared<views::ImagePane>(1, "../assets/right.jpg");
+    auto right_image_pane = std::make_shared<views::ImagePane>(1, "../assets/right.jpg");
     std::vector<Point> points = { Point(-0.8, 0.0) };
 
     auto left_point_layer = std::make_shared<views::PointLayer>(4, points);
     left_pane = std::make_shared<views::ZStack>(3, [=, this](views::LayoutContext &layout) {
         layout.add(left_image_pane);
         layout.add(left_point_layer);
+    });
+
+    auto line_layer = std::make_shared<LineLayer>(5);
+    right_pane = std::make_shared<views::ZStack>(3, [=, this](views::LayoutContext &layout) {
+        layout.add(right_image_pane);
+        layout.add(line_layer);
     });
 
     auto hstack = std::make_shared<views::HStack>(2, [=, this](views::LayoutContext &layout) {
